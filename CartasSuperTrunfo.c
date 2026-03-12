099b446c8bd7b6a645e8cb6a245be11293ebcbf9
Rafudos <rafaelbritoasdf@gmail.com>
2026-03-11 23:36:59 -0300

    CartasSuperTrunfo AVENTUREIRO.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,29 +8,29 @@ int main(){
     float Area;
     float PIB;
     int PontoTuristico;
+    float DensidadePopulacional;
+    float PIBPerCapita;
 
     printf("ESCOLHA SEU ESTADO (SIGLA): \n");
     scanf("%2s", Estado);
 
-    printf("ESCOLHA SUA CIDADE: \n");
-    getchar(); 
-    fgets(Cidade, sizeof(Cidade), stdin);
+    printf("ESCOLHA SUA CIDADE: \n"); 
+    scanf("%20s", Cidade);
 
     printf("QUAL A POPULACAO DA SUA CIDADE? \n");
     scanf("%d", &Populacao);
 
     printf("QUAL A AREA DA SUA CIDADE? \n");
     scanf("%f", &Area);
-    char buffer[50];
-    getchar();
-    fgets(buffer, sizeof(buffer), stdin);
-    sscanf(buffer, "%f", &Area);
-
+    
     printf("QUAL O PIB DA SUA CIDADE? \n");
     scanf("%f", &PIB);
 
     printf("QUANTOS PONTOS TURISTICOS TEM NA SUA CIDADE? \n");
     scanf("%d", &PontoTuristico);
+
+    DensidadePopulacional = Populacao / Area;
+    PIBPerCapita = PIB / Populacao;
 
     printf("\n--- DADOS INFORMADOS ---\n");
     printf("ESTADO: %s\n", Estado);
@@ -39,7 +39,8 @@ int main(){
     printf("AREA: %.2f\n", Area);
     printf("PIB: %.2f\n", PIB);
     printf("PONTOS TURISTICOS: %d\n", PontoTuristico);
+    printf("DENSIDADE POPULACIONAL: %.2f\n", DensidadePopulacional);
+    printf("PIB PER CAPITA: %.2f\n", PIBPerCapita);
 
     return 0;
 }
-
